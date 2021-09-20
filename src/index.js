e6dceaf15e799848319f6a1be75d67cc2bbf9201
Toni1993 <77364364+Toni1993@users.noreply.github.com>
2021-09-21 01:18:57 +0300

    hj

--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
-import menu from './js/fetchCountries'
+import { fetchCountry } from './js/fetchCountries'
+// console.log(fetchCountry)
 import debounce from 'lodash.debounce'
+import '@pnotify/core/dist/BrightTheme.css'
 
-import countriesTmp from './templates/countries.hbs'
-import countriesListTmp from './templates/countries-list.hbs'
+import { alert, notice, info, success, error } from '@pnotify/core'
+// or
+
+// Manually set the type.
+const myAlert = alert({
+  text: "I'm an alert.",
+  type: 'info',
+})
+
+// Automatically set the type.
+const myNotice = notice({
+  text: "I'm a notice.",
+})
+
+const myInfo = info({
+  text: "I'm an info message.",
+})
+
+const mySuccess = success({
+  text: "I'm a success message.",
+})
+
+const myError = error({
+  text: "I'm an error message.",
+})
+
+// import countriesTmp from './templates/countries.hbs'
+// import countriesListTmp from './templates/countries-list.hbs'
 
 const listRef = document.querySelector('#list')
-console.log(listRef)
+// console.log(listRef)
 const inputRef = document.querySelector('.input-text')
-inputRef.addEventListener('input', debounce(getCountries, 2000))
-function getCountries() {
+
+inputRef.addEventListener('input', debounce(getCountries, 500))
+function getCountries(e) {
   let getCountries = e.target.value.toLowerCase().trim()
-  console.log(getCountries)
+
+  fetchCountry(getCountries).then(data => {
+    listRef.innerHTML = ''
+    console.log(data)
+    const countries = data
+      .splice(-10)
+      .map(elem => {
+        return `
+    <li>
+       ${elem.name}
+       </li>
+     `
+      })
+      .join('')
+    listRef.insertAdjacentHTML('afterbegin', countries)
+  })
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
-BASE_URL: 'https://restcountries.eu/rest/v2/name/'
+const BASE_URL = 'https://restcountries.eu/rest/v2/name/'
 
-function fetchCountry(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json())
+export function fetchCountry(searchQuery) {
+  return fetch(`${BASE_URL}${searchQuery}`).then(response => response.json())
 }
